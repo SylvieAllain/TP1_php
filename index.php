583e580d68eadbe2fefe63e8b56d441ef90e2973
SylvieAllain <sylvie_allain93@hotmail.com>
2019-02-25 14:27:57 -0500

    chaaaaaaaaaaaaaaaarr

--- index.php
+++ index.php
@@ -1,8 +1,80 @@
-<?php echo "hello";
-echo "World!";
+<?php
 
+include "modeles/model_cars.php";
 
-function nnnn() {
+$cat_cars = (!empty($_POST['catCars'])) ? ($_POST['catCars']) : null;
+$submit_cat = (!empty($_POST['submit_cat'])) ? ($_POST['submit_cat']) : null;
+$car = (!empty($_POST['car'])) ? ($_POST['car']) : null;
+$submit = (!empty($_POST['submit'])) ? ($_POST['submit']) : null;
 
-}
+  function createSelectCategory($array_cars, $cat_aliment){
+    echo '<select name='.'"'.'catCars'.'"'.'>';
+    foreach($array_cars as $key=>$value){
+      if ($cat_cars == $key){
+        echo "<option selected value="."\""."$key"."\"".">" . "$key" . "</option>";
+      }
+      else{
+        echo "<option value="."\""."$key"."\"".">" . "$key" . "</option>";
+      }
+    }
+    echo "</select>";
+    echo "<input type=". "\"" . "submit" . "\"" . "name=" . "\"" . "submit_cat" . "\"". "value=" . "\"" . "envoyer". "\"". ">";
+  }
+  $arrayLength = count($array_cars[$cat_cars]);
+
+  function selectedCarModel($array_cars,$cat_cars,$car){
+    echo '<select name='.'"'.'car'.'"'.'>';
+    foreach($array_cars[$cat_cars] as $key=> $value){
+      if($array_cars[$cat_cars][$key] == $car){
+        echo "<option selected value="."\""."$value"."\"".">" . "$value" . "</option>";
+      }
+      else{
+        echo "<option value="."\""."$value"."\"".">" . "$value" . "</option>";
+      }
+    }
+    echo "</select>";
+    echo "<input type=". "\"" . "submit" . "\"" . "name=" . "\"" . "submit" . "\"". "value=" . "\"" . "envoyer". "\"". ">";
+  }
+
+  function validate_cat_car($cat_cars){
+    if(empty($cat_cars)){
+      throw new Exception ("Vous devez sélectionner une catégorie d'aliment");
+    }
+  }
+
+  function validate_car($car){
+    if(empty($car)){
+      throw new Exception ("Vous devez sélectionner un aliment");
+    }
+  }
+
+  //createMenu($array_aliments,$cat_aliment);
+
+  //include_once 'vues/accueil.php';
+
+  if(!empty($submit_cat)){
+    try{
+      validate_cat_aliment($cat_cars);
+    }
+    catch(Exception $e){
+      echo "Erreur : " . $e->getMessage();
+    }
+  }
+
+  if(!empty($submit)){
+    try{
+      validate_aliment($car);
+      //include_once "vues/manger.php";
+      header("location:vues/manger.php?aliment=$car");
+
+    }
+    catch(Exception $e){
+      echo "Erreur : " . $e->getMessage();
+    }
+  }
+
+createSelectCategory($array_cars,$cat_aliment);
+selectedCarModel($array_cars[$cat_aliment]);
+
+//include_once "vues/accueil.php";
 ?>
---
+++ modeles/model_cars.php
@@ -0,0 +1,8 @@
+<?php
+$array_cars = [
+  "Volkswagen" => ["Beetle","Jetta","Golf"],
+  "Pontiac" => ["Sunbird", "GTO","Tempest"],
+  "Ssangyong" => ["Korando","Musso","Rexton"],
+  "Lada" => ["Niva", "Granda", "Vesta"],
+];
+?>
