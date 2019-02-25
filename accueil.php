238ad0ef729b56d35834589a644e3dd5a9a34159
CascadaFTW <44841418+CascadaFTW@users.noreply.github.com>
2019-02-25 14:32:26 -0500

    Page accueuil
    Form + search bar

--- accueil.php
+++ accueil.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
-<html lang="foreach" dir="ltr">
+<html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title></title>
   </head>
   <body>
+    <form name="formulaire" action="" method="post">
+      <label for="brand"></span>Marque: </label>
+      <label for="model">Modèle: </label>
+      <br><br>
+      <label for="searchBar">Rechercher: </label>
+      <input type="text" name="searchBar" value="">
+
+    </form>
 
   </body>
 </html>
