b70b1c042a904f8e2d73fd7ebaa9e013c59e88d7
CascadaFTW <44841418+CascadaFTW@users.noreply.github.com>
2019-02-25 15:27:02 -0500

    Create accueil.php

---
+++ accueil.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form name="formulaire" action="" method="post">
+      <label for="brand"></span>Marque: </label>
+      <label for="model">Modèle: </label>
+      <br><br>
+      <label for="searchBar">Rechercher: </label>
+      <input type="text" name="searchBar" value="">
+
+    </form>
+
+  </body>
+</html>
