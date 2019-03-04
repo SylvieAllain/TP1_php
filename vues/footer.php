c33afa97af59aaa635d33c36dbbbc875e95b08a0
CascadaFTW <44841418+CascadaFTW@users.noreply.github.com>
2019-03-04 15:10:27 -0500

    Update footer.php

--- vues/footer.php
+++ vues/footer.php
@@ -5,17 +5,19 @@
     <title></title>
     <style media="screen">
       footer {
-        background-color: #384F34;    
+        background-color: #384F34;
       }
     </style>
   </head>
   <body>
     <footer>
-      <p class="footerFont"><span class="underline">Aucuns retours ou garanties possibles après achat</span> <br>
+      <p class="footerFont"><span class="underline">Aucun retour ou garantie possible après achat</span> <br>
           Copyright © Sylvie Allain et Cyrice Paradis <br>
           2019
       </p>
-      <a href="http://www.perdu.com/">Mentions légales ou pour nous poursuivre</a>
+      <a href="http://www.tinytuba.com/">Mentions légales</a>
+      &nbsp;&nbsp;
+      <a href="http://www.perdu.com/">Pour nous poursuivre</a>
     </footer>
   </body>
 </html>
