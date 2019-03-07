b68914598d26644abfb7399df1bbbc37fe6c6c10
CascadaFTW <44841418+CascadaFTW@users.noreply.github.com>
2019-03-07 12:35:17 -0500

    css

--- vues/banner.php
+++ vues/banner.php
@@ -14,18 +14,34 @@ Auteur: Sylvie Allain & Cyrice Paradis
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../css/banner.css">
+    <style media="screen">
+      body {
+            padding: 1em;
+            margin-left: 1em;
+            background-color: #384F34;
+            font-size: 30em;
+          }
+      h1 {
+            font-size: 4em;
+      }
+    </style>
   </head>
   <body>
-    <?php
-      if(empty($_GET["model"])) {
-        echo "<img src=\"images/banner.png\" alt=\"Belle auto citron.\">";
-      }
-      else {
-        echo "<img src=\"../images/banner.png\" alt=\"Belle auto citron.\">";
-      }
-     ?>
-    <h1>Voiture @Variée</h1>
+    <div class="row" id="carDisplayRowFinancing">
+      <div class="col-2">
+        <?php
+          if(empty($_GET["model"])) {
+            echo "<img src=\"images/banner.png\" alt=\"Belle auto citron.\">";
+          }
+          else {
+            echo "<img src=\"../images/banner.png\" alt=\"Belle auto citron.\">";
+          }
+         ?>
+      </div>
+      <div class="col">
+        <h1>Voiture @Variée</h1>
+      </div>
+    </div>
       <h2>La plus grande sélection de citrons provenant de tous les continents, sauf de l'Afrique...</h2>
   </body>
 </html>
