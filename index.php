7ef9b1907996bdf50269309da00888f786c3badc
CascadaFTW <44841418+CascadaFTW@users.noreply.github.com>
2019-03-14 17:06:53 -0400

    »EMAIL!!!

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ Auteur: Sylvie Allain & Cyrice Paradis
 -->
 
 <?php
-//TODO // réusiner le code pour qu'il ait moins de DRY. Tenter également de retirer tout les éléments globals.
 $pageTitle = "Accueil";
 include_once 'vues/banner.php';
 include "modeles/model_cars.php";
