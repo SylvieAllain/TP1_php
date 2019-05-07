30e1b4023cffa9287d3fa3130a79f56b3a9836e0
CascadaFTW <44841418+CascadaFTW@users.noreply.github.com>
2019-05-07 18:04:18 -0400

    Update enattendant.java

--- enattendant.java
+++ enattendant.java
@@ -1,174 +1,59 @@
-package TP2;
 
-import TP2_MAZE_SERVICES.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+	@Test
+	public void GIVEN_pathFinderCounterClockwise_WHEN_IsAskedToResolveAMastermindMaze_THEN_MazeIsResolve(){
 
-public class PathFinderClockwise {
-	
-	private List<Room> listOfDoomedRooms;
-	private Stack<Room> path;
-	
-	public PathFinderClockwise () {
-		this.listOfDoomedRooms = new ArrayList<Room>();
-		this.path = new Stack<Room>();
-		
+		final String FIRST_ROW =             "XXXXXXXXXXXXXXXXXXXX";
+        final String SECOND_ROW =            "XS                 X";
+        final String THIRD_ROW =             "X XXXXXXXXXXXXXXXXXX";
+        final String FOURTH_ROW =            "X         XXXXXXXXXX";
+        final String FIFTH_ROW =             "X XXXXXXX XXXX XXXXX";
+        final String SIXTH_ROW =             "X XXXXXXX XXXX     X";
+        final String SEVENTH_ROW =           "X XX         X XX XX";
+        final String EIGHT_ROW =             "X XXXXXXXXXX X  X XX";
+        final String NINETH_ROW =            "X       XXXX XX X XX";
+        final String TENTH_ROW =             "XXXXX XXXXXX    X XX";
+        final String ELEVENTH_ROW =		     "XX    X    X XXXX XX";
+        final String TWELVETH_ROW =          "XX XXXX XX XXXXXX XX";
+        final String THIRDTEENTH_ROW =       "XX      X          X";
+        final String FOURTEENTH_ROW =        "XXXXXXXXXXXXX XXXXXX";
+        final String FIFTHEENTH_ROW =        "XXXXXXXXXXXXX     EX";
+        final String SIXTEENTH_ROW =         "XXXXXXXXXXXXXXXXXXXX";
+        final String SEVENTEENTH_ROW =       "XXXXXXXXXXXXXXXXXX X";
+        final String EIGHTEENTH_ROW =        "XXXXXXXXXXXXXXXXXX X";
+        final String NINETEENTH_ROW =        "XXXXXXXXXXXXXXXXXX X";
+        final String TWENTYTH_ROW =          "XXXXXXXXXXXXXXXXXXXX";
+
+
+		final String MAZE = FIRST_ROW + SECOND_ROW + THIRD_ROW + FOURTH_ROW + FIFTH_ROW + SIXTH_ROW + SEVENTH_ROW + EIGHT_ROW + NINETH_ROW + TENTH_ROW + ELEVENTH_ROW + TWELVETH_ROW + THIRDTEENTH_ROW + FOURTEENTH_ROW + FIFTHEENTH_ROW + SIXTEENTH_ROW + SEVENTEENTH_ROW + EIGHTEENTH_ROW + NINETEENTH_ROW + TWENTYTH_ROW;
+
+		PathFinderCounterClockwise pathfinder = new PathFinderCounterClockwise();
+
+		//Act
+		final String ACTUAL_MAZE = pathfinder.pathToExitClockwise(MAZE);
+
+		//Assert
+		final String EXPECTED_FIRST_ROW =             "XXXXXXXXXXXXXXXXXXXX";
+        final String EXPECTED_SECOND_ROW =            "XS                 X";
+        final String EXPECTED_THIRD_ROW =             "XPXXXXXXXXXXXXXXXXXX";
+        final String EXPECTED_FOURTH_ROW =            "XP        XXXXXXXXXX";
+        final String EXPECTED_FIFTH_ROW =             "XPXXXXXXX XXXX XXXXX";
+        final String EXPECTED_SIXTH_ROW =             "XPXXXXXXX XXXX     X";
+        final String EXPECTED_SEVENTH_ROW =           "XPXX         X XX XX";
+        final String EXPECTED_EIGHT_ROW =             "XPXXXXXXXXXX X  X XX";
+        final String EXPECTED_NINETH_ROW =            "XPPPPP  XXXX XX X XX";
+        final String EXPECTED_TENTH_ROW =             "XXXXXPXXXXXX    X XX";
+        final String EXPECTED_ELEVENTH_ROW =		  "XXPPPPXPPPPX XXXX XX";
+        final String EXPECTED_TWELVETH_ROW =          "XXPXXXXPXXPXXXXXX XX";
+        final String EXPECTED_THIRDTEENTH_ROW =       "XXPPPPPPX PPPP     X";
+        final String EXPECTED_FOURTEENTH_ROW =        "XXXXXXXXXXXXXPXXXXXX";
+        final String EXPECTED_FIFTHEENTH_ROW =        "XXXXXXXXXXXXXPPPPPEX";
+        final String EXPECTED_SIXTEENTH_ROW =         "XXXXXXXXXXXXXXXXXXXX";
+        final String EXPECTED_SEVENTEENTH_ROW =       "XXXXXXXXXXXXXXXXXX X";
+        final String EXPECTED_EIGHTEENTH_ROW =        "XXXXXXXXXXXXXXXXXX X";
+        final String EXPECTED_NINETEENTH_ROW =        "XXXXXXXXXXXXXXXXXX X";
+        final String EXPECTED_TWENTYTH_ROW =          "XXXXXXXXXXXXXXXXXXXX";
+
+        final String EXPECTED_MAZE = EXPECTED_FIRST_ROW + EXPECTED_SECOND_ROW + EXPECTED_THIRD_ROW + EXPECTED_FOURTH_ROW + EXPECTED_FIFTH_ROW + EXPECTED_SIXTH_ROW + EXPECTED_SEVENTH_ROW + EXPECTED_EIGHT_ROW + EXPECTED_NINETH_ROW + EXPECTED_TENTH_ROW + EXPECTED_ELEVENTH_ROW + EXPECTED_TWELVETH_ROW + EXPECTED_THIRDTEENTH_ROW + EXPECTED_FOURTEENTH_ROW + EXPECTED_FIFTHEENTH_ROW + EXPECTED_SIXTEENTH_ROW + EXPECTED_SEVENTEENTH_ROW + EXPECTED_EIGHTEENTH_ROW + EXPECTED_NINETEENTH_ROW + EXPECTED_TWENTYTH_ROW;
+
+		assertEquals(EXPECTED_MAZE, ACTUAL_MAZE);
 	}
-	
-	
-	public String pathToExitClockwise (String fileContent) {
-		
-		try {
-			//Room[][] rooms = new Room[MazeCreator.NUMBER_OF_ROWS][MazeCreator.NUMBER_OF_COLUMNS];
-			//this.createMaze(fileContent,rooms);
-            Room room = MazeCreator.getStartRoom(fileContent);
-            //this.path.push(room);
-            
-            while(room.type != Room.Type.Exit) {
-            	
-            	Room lastRoomVisited = null;
-            	if(!path.isEmpty()) {
-            		lastRoomVisited = this.path.peek();
-            	}
-            	
-            	//room = getRoomToVerify(lastRoomVisited,rooms);
-            	boolean isMovedFromRoom = false;
-                
-                if(room.up.type != Room.Type.Wall) {
-                	boolean isWrongPath = this.isRoomVisited(room.up, listOfDoomedRooms);
-                	if(!isWrongPath && !room.up.equals(lastRoomVisited)) {
-                		this.path.push(room);
-                		room = room.up;
-                        isMovedFromRoom = true;
-                	}
-                }
-                if(room.right.type != Room.Type.Wall && !isMovedFromRoom ) {
-            		boolean isRoomVisited = this.isRoomVisited(room.right, listOfDoomedRooms);
-            		if(!isRoomVisited && !room.right.equals(lastRoomVisited)) {
-            			this.path.push(room);
-        				 room = room.right;
-            			 isMovedFromRoom = true;
-            		}
-                }
-                if(room.down.type != Room.Type.Wall && !isMovedFromRoom) {
-                	boolean isRoomVisited = this.isRoomVisited(room.down, listOfDoomedRooms);
-                	if(!isRoomVisited && !room.down.equals(lastRoomVisited)) {
-                		this.path.push(room);
-                		room = room.down;
-                        isMovedFromRoom = true;
-                	}
-                }
-            	if(room.left.type != Room.Type.Wall && !isMovedFromRoom) {
-                	boolean isRoomVisited = this.isRoomVisited(room.left, listOfDoomedRooms);
-                	if(!isRoomVisited && !room.left.equals(lastRoomVisited)) {
-                		this.path.push(room);
-                		room = room.left;
-                        isMovedFromRoom = true;
-                	}
-                }
-                if(!isMovedFromRoom){
-	        		this.listOfDoomedRooms.add(room);
-	        		room = this.path.peek();
-	        		this.path.pop();
-            	}
-            }
-            
-		} 
-		catch (MazeLengthNotEqualToExpectedValuesException e) {
-		
-		}
-		
-		String newFileContent = this.createNewFileContent(path);
-		return newFileContent;
-	}
-	
-	private void createMaze (String fileContent, Room[][] rooms) {
-		
-		for (int i = 0; i < MazeCreator.NUMBER_OF_ROWS; i++) {
-			for (int j = 0; j < MazeCreator.NUMBER_OF_COLUMNS; j++) {
-				Room room =  new Room(fileContent.charAt(j + (i*MazeCreator.NUMBER_OF_ROWS)));
-				rooms[i][j] = room;
-			}
-		}
-		
-		addRoomToEachDirection(rooms);
-	}
-	
-	private void addRoomToEachDirection(Room[][] rooms) {
-		
-		for (int i = 0; i < MazeCreator.NUMBER_OF_ROWS; i++) {
-			for (int j = 0; j < MazeCreator.NUMBER_OF_COLUMNS; j++) {
-				Room roomBuffer = rooms[i][j];
-				if(i != 0) {
-					roomBuffer.up = rooms[i - 1][j];
-				}
-				if(j != MazeCreator.NUMBER_OF_COLUMNS - 1) {
-					roomBuffer.right = rooms[i][j + 1];
-				}
-				if(i != MazeCreator.NUMBER_OF_ROWS - 1) {
-					roomBuffer.down = rooms[i + 1][j];
-				}
-				if(j != 0) {
-					roomBuffer.down = rooms[i][j - 1];
-				}
-				rooms[i][j] = roomBuffer;
-			}
-		}
-	}
-	
-	private boolean isRoomVisited (Room room, List<Room> listOfDoomedRooms) {
-		boolean isRoomVisited = false;
-		for(Room element : listOfDoomedRooms) {
-    		if (element.equals(room)) {
-    			isRoomVisited = true;
-    		}
-		}
-		return isRoomVisited;
-	}
-	
-	private String createNewFileContent(Stack<Room> roomPath) {
-		List<Room> arrayBuffer = new ArrayList<Room>();
-		
-		createArrayListBuffer(roomPath,arrayBuffer);
-		
-		ArrayList<Room> realList = new ArrayList<Room>();
-		
-		createRealArrayList(arrayBuffer,realList);
-		
-		Room startRoom = realList.get(0);
-		
-		String newFileContent = MazeCreator.getStringMazeWithPathToExitRoom(startRoom, realList);
-		
-		return newFileContent;
-	}
-	
-	private void createArrayListBuffer(Stack<Room> roomPath, List<Room> arrayBuffer) {
-		
-		for (Room element: roomPath) {
-			arrayBuffer.add(element);
-		}
-	}
-	
-	private void createRealArrayList (List<Room> arrayBuffer, ArrayList<Room> realList) {
-		
-		int arrayBufferLength = arrayBuffer.size();
-		
-		for(int i = arrayBufferLength - 1; i > -1; i--) {
-			realList.add(arrayBuffer.get(i));
-		}
-	}
-	
-	private Room getStartRoomInArray (Room room, Room[][] rooms) {
-		Room startRoom = null;
-		for (int i = 0; i < MazeCreator.NUMBER_OF_ROWS; i++) {
-			for (int j = 0; j < MazeCreator.NUMBER_OF_COLUMNS; j++) {
-				if(room.equals(rooms[i][j])) {
-					startRoom = room;
-				}
-			}
-		}
-		return startRoom;
-	}
-	
-}
